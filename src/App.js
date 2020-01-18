27abe6774cd4e66f4bd6117cbd42fdb2ed277b08
Alexsandro Silveira <alexsandro@tinotech.com.br>
2020-01-18 10:17:12 -0300

    crud genero

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import Header from './Header';
+import Generos from './Genero/Generos';
+import NovoGenero from './Genero/NovoGenero';
+import EditarGenero from './Genero/EditarGenero';
 import Axios from 'axios';
 import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom';
 
+
 const Home = () => {
   return <h1>Home</h1>;
-}
-
-const Generos = () => {
-  return <h1>Generos</h1>;
 }
 
 function App() {
@@ -28,7 +28,9 @@ function App() {
       <div>
         <Header />
         <Route path="/" exact component={Home} />
-        <Route path="/generos" component={Generos} />
+        <Route path="/generos/:id" exact component={EditarGenero} />
+        <Route path="/generos/novo" exact component={NovoGenero} />
+        <Route path="/generos" exact component={Generos} />
         <pre>{JSON.stringify(data)}</pre>
       </div>
     </Router>
